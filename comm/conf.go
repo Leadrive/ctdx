f24ac2559e2ceec482b4dc82267d957a87d65f11
陈俊州 <chenjzh@qdum.com>
2017-11-28 14:03:16 +0800

    调整配置文件

--- comm/conf.go
+++ comm/conf.go
@@ -6,29 +6,33 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+type CUrls struct {
+	StockFin string `toml:"stock_fin"`
+	FinListFile string `toml:"fin_list_file"`
+}
+
+type CFiles struct {
+	StockList string `toml:"stock_list"`
+	StockBonus string `toml:"stock_bonus"`
+	StockDay string `toml:"stock_day"`
+	StockMin string `toml:"stock_min"`
+	StockReport string `toml:"stock_report"`
+}
+
+type BaseApp struct {
+	Mode string `toml:"mode"`
+	DataPath string `toml:"data_path"`
+	Logger struct {
+		Level string `toml:"level"`
+		Name  string `toml:"name"`
+	} `toml:"logger"`
+}
+
 type Conf struct {
 	App struct {
-		Mode string `toml:"mode"`
-		DataPath string `toml:"data_path"`
-
-		Logger struct {
-			Level string `toml:"level"`
-			Name string `toml:"name"`
-		} `toml:"logger"`
-
-		Urls struct {
-			StockFin string `toml:"stock_fin"`
-			FinListFile string `toml:"fin_list_file"`
-		} `toml:"urls"`
-
-		Files struct {
-			StockList string `toml:"stock_list"`
-			StockBonus string `toml:"stock_bonus"`
-			StockDay string `toml:"stock_day"`
-			StockMin string `toml:"stock_min"`
-			StockReport string `toml:"stock_report"`
-		} `toml:"files"`
-
+		BaseApp
+		Urls CUrls    `toml:"urls"`
+		Files CFiles  `toml:"files"`
 	} `toml:"app"`
 
 	Tdx struct {
